dc67d799fbf47d3b5fd2f25505a50bea1c0d7f8c
Gabriel Ribeiro Camargo Ferreira <gabrielxp@hotmail.com>
2017-08-05 14:24:52 -0300

    Prova Back-End.

---
+++ ProvaBackEnd_07_17/Models/Noticias/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProvaBackEnd_07_17.Models.Noticias
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        public virtual IList<Noticia> Noticias { get; set; }
+    }
+}
---
+++ ProvaBackEnd_07_17/Models/Noticias/NoticiaItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProvaBackEnd_07_17.Models.Noticias
+{
+    public class NoticiaItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TipoId { get; set; }
+
+        public int NoticiaId { get; set; }
+
+        [Display(Name = "Valor")]
+        public string Valor { get; set; }
+
+        public virtual Tipo Tipo { get; set; }
+
+        public virtual Noticia Noticia { get; set; }
+
+    }
+}
